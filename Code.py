5aa8213925db63a34b9e792443adedb33f759498
girishmohakar <girishmohakarentc@gmail.com>
2025-06-21 00:18:00 +0530

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,9 @@
+name = input(" what is your name ")
+if name == "girish":
+   print(" Hello! I'm Girish")
+   print(" Age: 20")
+   print(" Favorite Quote: 'Never stop learning, because life never stops teaching.'")
+   print(" Currently Learning: Python Programming")
+   print(" 21-Day Goal: Reach Intermediate Python & build cool projects!")
+else:
+    print("You are not Girish, access denied Sorry! ")
